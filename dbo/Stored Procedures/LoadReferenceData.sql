c46ef77bcd282bb7d5a5865a96ce9cd4b7740826
Vasileios Boukonis <vasiliss_b@hotmail.com>
2025-07-28 23:25:00 +0300

    added reference data store procedure

---
+++ dbo/Stored Procedures/LoadReferenceData.sql
@@ -0,0 +1,24 @@
+﻿CREATE PROCEDURE [dbo].[LoadReferenceData]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT
+        (
+            SELECT Id, Name FROM Formats ORDER BY Name
+            FOR JSON PATH
+        ) AS Formats,
+        (
+            SELECT Id, Name FROM Countries ORDER BY Name
+            FOR JSON PATH
+        ) AS Countries,
+        (
+            SELECT Id, Name FROM Genres ORDER BY Name
+            FOR JSON PATH
+        ) AS Genres,
+        (
+            SELECT Id, Name FROM Styles ORDER BY Name
+            FOR JSON PATH
+        ) AS Styles
+    FOR JSON PATH, WITHOUT_ARRAY_WRAPPER;
+END;
